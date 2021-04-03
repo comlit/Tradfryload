c19f4bbf487122fb0a84693b6202ab179bd8c79c
Fabian <1803fabian@gmail.com>
2021-04-03 18:10:26 +0200

    added basic layout added tradfry backend added shared prefs

--- app/src/main/java/com/lit/tradfryload/MainActivity.java
+++ app/src/main/java/com/lit/tradfryload/MainActivity.java
@@ -2,13 +2,56 @@ package com.lit.tradfryload;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+
+import nl.stijngroenen.tradfri.device.Device;
+import nl.stijngroenen.tradfri.device.Gateway;
+import nl.stijngroenen.tradfri.util.Credentials;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button testbtn;
+    SharedPreferences prefs;
+    Credentials creds;
+    Gateway gateway;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testbtn = findViewById(R.id.button);
+
+        prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        creds = new Credentials(prefs.getString("identity", null), prefs.getString("key", null));
+        if(creds.getIdentity() == null || creds.getKey() == null)
+        {
+            try {
+                creds = Tradfry.newCreds();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("identity", creds.getIdentity());
+            editor.putString("key", creds.getKey());
+            editor.apply();
+        }
+
+        testbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                gateway.connect(creds);
+                gateway.getDevices();
+                System.out.println("DeviceNames:");
+                for(Device dev: gateway.getDevices())
+                    System.out.println(dev.getName());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/lit/tradfryload/Tradfry.java
@@ -0,0 +1,71 @@
+package com.lit.tradfryload;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import nl.stijngroenen.tradfri.device.Device;
+import nl.stijngroenen.tradfri.device.Gateway;
+import nl.stijngroenen.tradfri.util.Credentials;
+
+public class Tradfry
+{
+    String gwIP;
+    Gateway gateway;
+    public Tradfry(String ip) throws FileNotFoundException
+    {
+        gwIP = ip;
+        gateway = new Gateway(gwIP);
+        login(newCreds());
+    }
+
+    static public Credentials newCreds() throws FileNotFoundException
+    {
+        System.out.println("Gettin new Creds");
+        Credentials credentials = new Gateway("192.168.178.27").connect("UlBKejOQzM99rnL0");
+        String identity = credentials.getIdentity();
+        String key = credentials.getKey();
+        return new Credentials(identity, key);
+    }
+
+    public void login(Credentials creds)
+    {
+        System.out.println("Loggin in");
+        gateway.connect(creds);
+    }
+
+    public void run() throws FileNotFoundException
+    {
+        login(newCreds());
+        System.out.println("Printing DeviceNames:");
+        for(Device dev: gateway.getDevices())
+            System.out.println(dev.getName());
+        turn9off();
+    }
+
+    public ArrayList<Device> getDevices()
+    {
+        ArrayList<Device> list = new ArrayList<>();
+        for(Device dev: gateway.getDevices())
+            list.add(dev);
+        return list;
+    }
+
+    public void test()
+    {
+        gateway.getDevices()[4].toLight().setOn(false);
+    }
+
+    public  void turnOn(int dev)
+    {
+        gateway.getDevices()[dev].toLight().setOn(true);
+    }
+
+    public  void turnOff(int dev)
+    {
+        gateway.getDevices()[dev].toLight().setOn(false);
+    }
+
+    public void turn9on(){gateway.getDevices()[9].toLight().setOn(true);};
+
+    public void turn9off(){gateway.getDevices()[9].toLight().setOn(false);};
+}
